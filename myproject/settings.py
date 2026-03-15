a3e357d4ec6414372f7749458cd8760e1acc109b
Vedang Dhuri <netvedang@gmail.com>
2026-03-15 20:00:49 +0530

    Fix CSRF trusted origins for Hugging Face

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,6 +34,12 @@ else:
     # Allow all in development
     ALLOWED_HOSTS.append("*")
 
+# Hugging Face CSRF Trusted Origins
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.hf.space",
+    "https://*.huggingface.co",
+]
+
 AUTH_USER_MODEL = "myapp.CustomUser"
 
 INSTALLED_APPS = [
